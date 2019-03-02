ae90584639ab7bde04b371eabcec8f6dd5898ba1
aspiringguru <bmatthewtaylor@gmail.com>
2019-03-02 17:46:20 +1000

    L9 @ 15:38, problem identified with event.attendees.length in EventShow.vue

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,6 +1,29 @@
 <template>
-  <h1>Showing event {{ event.title }}</h1>
+  <div>
+    <div class="event-header">
+      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+      <h1 class="title">{{ event.title }}</h1>
+      <h5>Organized by {{ event.organizer }}</h5>
+      <h5>Category: {{ event.category }}</h5>
+    </div>
+    <BaseIcon name="map">
+      <h2>Location</h2>
+    </BaseIcon>
+    <address>{{ event.location }}</address>
+    <h2>Event details</h2>
+    <p>{{ event.description }}</p>
+    <h2>Attendees
+      <!-- <span class="badge -fill-gradient">{{ event.attendees.length }}</span> -->
+      <span class="badge -fill-gradient">{{ event.attendees.length }}</span>
+    </h2>
+    <ul class="list-group">
+      <li v-for="(attendee, index) in event.attendees" :key="index" class="list-item">
+        <b>{{ attendee.name }}</b>
+      </li>
+    </ul>
+  </div>
 </template>
+
 <script>
 import EventService from '@/services/EventService.js'
 
@@ -15,6 +38,13 @@ export default {
     EventService.getEvent(this.id)
       .then(response => {
         //console.log('response.data = ', response.data)
+        console.log('response.data.location:', response.data.location)
+        console.log('response.data.attendees:', response.data.attendees)
+        console.log('response.data.attendees[0]:', response.data.attendees[0])
+        console.log(
+          'response.data.attendees.length:',
+          response.data.attendees.length
+        )
         this.event = response.data
       })
       .catch(error => {
@@ -23,3 +53,24 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.location {
+  margin-bottom: 0;
+}
+.location > .icon {
+  margin-left: 10px;
+}
+.event-header > .title {
+  margin: 0;
+}
+.list-group {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+.list-group > .list-item {
+  padding: 1em 0;
+  border-bottom: solid 1px #e5e5e5;
+}
+</style>
