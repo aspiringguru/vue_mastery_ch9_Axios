9ea2dce8839f4cba3e57740879f26f78bc1093a7
aspiringguru <bmatthewtaylor@gmail.com>
2019-03-02 16:31:41 +1000

    L9 @ 1015,api calls with Axios to json server

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,16 +1,33 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <EventCard/>
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
+import axios from 'axios'
 
 export default {
   components: {
     EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    axios
+      .get('http://localhost:3000/events') // Call out to this URL
+      .then(response => {
+        console.log('response.data = ', response.data)
+        this.events = response.data
+      })
+      .catch(error => {
+        console.log('There was an error', error.response) // If an error is returned log it to the console
+      })
   }
 }
 </script>
