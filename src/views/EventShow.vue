379b9aecfe77602a5e5fcef6a5c0ff0a9c9c8a61
aspiringguru <bmatthewtaylor@gmail.com>
2019-03-02 17:13:59 +1000

    L9 @ 1516, added second api call in EventShow

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,8 +1,25 @@
 <template>
-  <h1>Showing event #{{ id }}</h1>
+  <h1>Showing event {{ event.title }}</h1>
 </template>
 <script>
+import EventService from '@/services/EventService.js'
+
 export default {
-  props: ['id']
+  props: ['id'],
+  data() {
+    return {
+      event: {}
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then(response => {
+        //console.log('response.data = ', response.data)
+        this.event = response.data
+      })
+      .catch(error => {
+        console.log('There was an error', error.response) // If an error is returned log it to the console
+      })
+  }
 }
 </script>
